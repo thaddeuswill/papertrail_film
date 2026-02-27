8f9190384798153b2a2800604620d603ebb6f872
thaddeuswill <122293354+thaddeuswill@users.noreply.github.com>
2026-02-27 13:35:00 -0600

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [react()],
+  root: __dirname,
+  build: {
+    outDir: "dist",
+  },
+});
